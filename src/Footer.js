7d382911a2dc358b8880264d41628aaabb1461d7
Mark Meloy <meloymark@johndeere.com>
2020-08-25 11:05:28 -0500

    worked on head and footer CSS

--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./Footer.css";
 import { Route, Link, Switch } from "react-router-dom";
 import { waitForElementToBeRemoved } from "@testing-library/react";
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        
-          <footer className="footer">
-            <h5>copyright 2020 MJD LLC</h5>
-            <h3>Powered by MovieDB</h3>
-          </footer>
-        
-      </div>
+      <footer className="footer">
+        <h5>copyright 2020 MJD LLC</h5>
+        <h5>
+          This product uses the TMDb API but is not endorsed or certified by
+          TMDb
+        </h5>
+      </footer>
     );
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import "./App.css"
+import "./Header.css"
 import { Route, Link, Switch } from "react-router-dom";
 
 
@@ -7,7 +7,7 @@ class Header extends Component{
     render() {
         return (
           <div className="header">
-            <h1>Movie DB </h1>
+            <h1 id="title">Movie DB </h1>
             <nav>
               <Link to="/">Back to Movie DB Home Page</Link>
             </nav>
